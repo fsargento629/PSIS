931c3f79998058c506f0a3827c69923f34d3280a
fsargento629 <sargento228@gmail.com>
2020-06-03 15:37:10 +0100

    commneted  server.h

--- Server/server.h
+++ Server/server.h
@@ -7,24 +7,17 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-/*typedef struct board_data_struct
-{
-   int board_size[2];
-   game_object_struct** board;
-}board_data_struct;*/
-
-
 
 game_object_struct** board; // board global variable
-int board_size[2];
-int maxplayers;
-int* client_fd_list;
-int* client_score_fd;
-int* superpacman_tokens;
-int player_connections;
-pthread_mutex_t board_lock;
-board_struct board_data;
-int*scores;
+int board_size[2]; 
+int maxplayers; 
+int* client_fd_list; //list of client socket fd's
+int* client_score_fd; // list of client socket fd's for socre comms
+int* superpacman_tokens; //tokens for every player's pacman
+int player_connections; // //numbers of connected players
+pthread_mutex_t board_lock; //to lock all board related functions
+board_struct board_data; // stores the board pointer and the size of the board
+int*scores; // score for every client
 
 
 #endif
